1ecb708f0185b94d9f42b5839354c2a1986439fd
ThanhTrieu <nguyenthanhtrieu90@gmail.com>
2023-10-28 11:27:47 +0700

    update code

--- Tranning/Controllers/LoginController.cs
+++ Tranning/Controllers/LoginController.cs
@@ -1,12 +1,31 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Tranning.Models;
+using Tranning.Queries;
 
 namespace Tranning.Controllers
 {
     public class LoginController : Controller
     {
+        [HttpGet]
         public IActionResult Index()
+        { 
+            LoginModel model = new LoginModel();
+            return View(model);
+        }
+
+        [HttpPost]
+        public IActionResult Index(LoginModel model)
         {
-            return View();
+            model = new LoginQueries().CheckLoginUser(model.Username, model.Password);
+            if (string.IsNullOrEmpty(model.UserID) || string.IsNullOrEmpty(model.Username))
+            {
+                // dang nhap linh tinh - khong dung tai khoan trong database
+                ViewData["MessageLogin"] = "Account invalid";
+                return View(model);
+            }
+            // cho chuyen vao trang hom
+
+            return RedirectToAction(nameof(HomeController.Index), "Home");
         }
     }
 }
